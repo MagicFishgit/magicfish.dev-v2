7fa946afec715489e5babd3f47ee4bc0d34900eb
Rudi Visagie <rudivisagie@icloud.com>
2022-11-08 20:48:56 +0200

    added some styles.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 // Import Components and Containers.
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
+import './App.scss';
 
 
 const App = () => {
