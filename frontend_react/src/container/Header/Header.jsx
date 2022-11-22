6d4335678f6da6b279d69526a43b879d4c069bea
Rudi Visagie <rudivisagie@icloud.com>
2022-11-22 20:40:22 +0200

    finished initial header styles.

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
       </motion.div>
 
       <motion.div variant={scaleVariants} whileInView={scaleVariants.whileInView} className='app__header-circles'>
-          {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          {[images.javascript, images.redux, images.sass].map((circle, index) => (
             <div className='circle-cmp app__flex' key={`circle-${index}`}>
               <img src={circle} alt="circles" />
             </div>
